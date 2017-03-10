ee83e65202db9f5a9c53e911eb258c8880e76af2
Helio Perroni Filho <mail@xperroni.me>
2017-03-09 21:07:52 -0300

    feature: RMSE implementation
    Added an implementation of the RMSE performance metric.

--- src/tools.cpp
+++ src/tools.cpp
@@ -5,17 +5,26 @@ Tools::Tools() {}
 
 Tools::~Tools() {}
 
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
-}
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
+  VectorXd rmse(4);
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+  rmse << 0, 0, 0, 0;
+  int n = estimations.size();
+  for(int i = 0; i < n; ++i) {
+    const VectorXd &a = estimations[i];
+    const VectorXd &b = ground_truth[i];
+
+    VectorXd c = a - b;
+    VectorXd d2 = c.array() * c.array();
+    rmse += d2;
+  }
+
+  //calculate the mean
+  rmse /= n;
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
