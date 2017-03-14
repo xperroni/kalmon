47eec313516db6e4cc1ddccff85e2e3999ce99a3
Helio Perroni Filho <mail@xperroni.me>
2017-03-13 22:15:47 -0300

    feature: Parameter tunning
    Kalman filter parameters were tuned so required accuracy levels were
    achieved on the assignment datasets.

--- src/settings.cpp
+++ src/settings.cpp
@@ -5,14 +5,15 @@ namespace kalmon
 
 Settings::Settings():
   s_P0(0.01),
-  s_Qx(0.01),
-  s_Qy(0.01),
-  s_px(0.01),
-  s_py(0.01),
-  s_r(0.01),
-  s_d(0.01),
-  s_v(0.01)
+  s_Qx(10),
+  s_Qy(8),
+  s_px(0.0225),
+  s_py(0.02),
+  s_r(0.1),
+  s_d(0.1),
+  s_v(0.1)
 {
+  // Nothing to do.
 }
 
 Settings &getSettings()
